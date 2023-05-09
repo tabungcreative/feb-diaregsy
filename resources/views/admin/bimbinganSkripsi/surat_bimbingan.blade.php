988e6351eb7c338122990ca4771f99a91c009876
Golummn <riza.sh15@gmail.com>
2023-05-09 10:29:59 +0700

    update surat

--- resources/views/admin/bimbinganSkripsi/surat_bimbingan.blade.php
+++ resources/views/admin/bimbinganSkripsi/surat_bimbingan.blade.php
@@ -115,6 +115,7 @@
                 </tr>
             </tbody>
         </table>
+        <br>
         <img src="img/footer-kop.png" width="100%"/>
     </div>
 
@@ -170,6 +171,7 @@
                 </tr>
             </tbody>
         </table>
+        <br>
         <img src="img/footer-kop.png" width="100%"/>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
