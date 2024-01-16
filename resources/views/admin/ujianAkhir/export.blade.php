431410be3fc839dc97e164593b08379fb5520b52
Ahmad Rifai <ahmmd.riffai@gmail.com>
2024-01-16 11:29:55 +0700

    delete none table tag

--- resources/views/admin/ujianAkhir/export.blade.php
+++ resources/views/admin/ujianAkhir/export.blade.php
@@ -1,30 +1,32 @@
 <html>
-<style>
-    table {
-        width: 100%;
-        border: 1px solid #000;
-    }
-    th {
-        text-align: center;
-        color: #e9aa0b;
-    }
-</style>
-<br>
-<br>
-<br>
 <table>
-    <thead>
     <tr>
-        <th>No</th>
-        <th>Nim</th>
-        <th>Nama</th>
-        <th>Prodi</th>
-        <th>Nomer Telepon</th>
-        <th>Jenis Pendaftaran</th>
-        <th>Bukti Pembayaran</th>
-        <th>Keterangan</th>
-        <th>Status</th>
+        <td>No</td>
+        <td>Nim</td>
+        <td>Nama</td>
+        <td>Prodi</td>
+        <td>Nomer Telepon</td>
+        <td>NIK</td>
+        <td>Tempat Lahir</td>
+        <td>Tanggal Lahir</td>
+        <td>Judul Tugas Akhir</td>
+        <td>Pembimbing 1</td>
+        <td>Pembimbing 2</td>
+        <td>Verifikasi</td>
+        <td>Status</td>
+        <td>Tanggal Ujian</td>
+        <td>Keterangan</td>
+        <td>Bukti Pembayaran</td>
+        <td>File Skripsi</td>
+        <td>Ijazah Terakhir</td>
+        <td>Transkrips Nilai</td>
+        <td>Akta Kelahiran</td>
+        <td>Kartu Keluarga</td>
+        <td>KTP</td>
+        <td>Lembar Bimbingan</td>
+        <td>Pembayaran Semester Akhir</td>
+        <td>Sertifikat</td>
     </tr>
-</table>
 
+</table>
 </html>
