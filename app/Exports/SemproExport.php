9427b3f518a5d4da8f16f1cf4829f331afdba32d
Ahmad Rifai <ahmmd.riffai@gmail.com>
2024-11-03 16:18:16 +0700

    fix export sempro

--- app/Exports/SemproExport.php
+++ app/Exports/SemproExport.php
@@ -5,13 +5,15 @@ namespace App\Exports;
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
 use App\Models\Sempro;
+use App\Models\TahunAjaran;
 
 class SemproExport implements FromView
 {
     public function view(): View
     {
+        $tahunAjaran = TahunAjaran::where('is_active', 1)->first();
         return view('admin.sempro.export', [
-            'sempro' => Sempro::all()
+            'sempro' => Sempro::where('tahun_ajaran_id', $tahunAjaran->id)->get()
         ]);
     }
 }
